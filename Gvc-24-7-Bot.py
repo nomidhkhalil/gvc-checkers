3d8f72dcd8cc04b4f3b6f73eba77f23082b08378
nomidhkhalil <62396704+nomidhkhalil@users.noreply.github.com>
2026-08-28 10:41:43 +0500

    Update Gvc-24-7-Bot.py

--- Gvc-24-7-Bot.py
+++ Gvc-24-7-Bot.py
@@ -7,9 +7,8 @@ EVENT = os.getenv("EVENT_NAME", "")
 
 def send(msg):
     try:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        r = requests.get(url, params={"chat_id": CHAT_ID, "text": msg}, timeout=15)
-        print(f"Telegram sent: {r.status_code}")
+        BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
     except Exception as e:
         print(f"Telegram error: {e}")
 
